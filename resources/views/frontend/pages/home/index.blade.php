2d002b1b25045714233165450bbe9539ae7d8128
rickydamarsaputra <retrocode.rc@gmail.com>
2023-11-16 18:40:22 +0700

    done: halaman utama

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -5,4 +5,15 @@
 @endpush
 
 @section('content')
+<section id="home" class="container-fluid" desktop="{{asset('assets/media/uploads/images/banner_tentangkami.jpg')}}"
+  tablet="{{asset('assets/media/uploads/images/banner-tentang-kami-m.jpg')}}"
+  mobile="{{asset('assets/media/uploads/images/banner-tentang-kami-m.jpg')}}">
+  <div class="row">
+    <div class="col-md-6 col-lg-6 visible-md visible-lg" style="height:100%;">
+      <div class="quotes">
+        <p id="quote">&nbsp;</p>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
