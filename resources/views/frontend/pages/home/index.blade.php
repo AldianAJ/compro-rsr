e14376d2ef287921603ab10f81bdb88e349fef14
krisnajufer <krisnajufer07@gmail.com>
2023-11-26 21:57:59 +0700

    finish: video

--- resources/views/frontend/pages/home/index.blade.php
+++ resources/views/frontend/pages/home/index.blade.php
@@ -5,7 +5,7 @@
 @endpush
 
 @section('content')
-<section id="home" class="container-fluid" desktop="{{asset('assets/media/uploads/images/banner_tentangkami.jpg')}}"
+    {{-- <section id="home" class="container-fluid" desktop="{{asset('assets/media/uploads/images/banner_tentangkami.jpg')}}"
   tablet="{{asset('assets/media/uploads/images/banner-tentang-kami-m.jpg')}}"
   mobile="{{asset('assets/media/uploads/images/banner-tentang-kami-m.jpg')}}">
   <div class="row">
@@ -15,5 +15,8 @@
       </div>
     </div>
   </div>
-</section>
+</section> --}}
+    <video autoplay="autoplay" loop="loop" muted="muted" style="-o-filter: blur(0); filter: blur(0);">
+        <source src="{{ asset('assets/video-drone.mp4') }}" type="video/mp4" />
+    </video>
 @endsection
